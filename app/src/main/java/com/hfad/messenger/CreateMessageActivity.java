4c933a042ead7efb1d3a2baed4642e39ba9dcb8c
Aaron Harvey <harvnlenny@gmail.com>
2016-02-12 20:24:38 -0500

    Create 1st Activity

--- app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -2,8 +2,7 @@ package com.hfad.messenger;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class CreateMessageActivity extends ActionBarActivity {
 
@@ -13,25 +12,8 @@ public class CreateMessageActivity extends ActionBarActivity {
         setContentView(R.layout.activity_create_message);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_create_message, menu);
-        return true;
-    }
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
