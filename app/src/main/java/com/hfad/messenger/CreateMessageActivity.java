c364f1f88fe6e91d743f6c4d928bc01bcea1d75a
Aaron Harvey <harvnlenny@gmail.com>
2016-02-12 22:38:55 -0500

    Pass data from createMessage to receiveMessage

--- app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends ActionBarActivity {
 
@@ -15,7 +16,10 @@ public class CreateMessageActivity extends ActionBarActivity {
 
     //Call onSendMessage() when the button is clicked
     public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
 
     }
